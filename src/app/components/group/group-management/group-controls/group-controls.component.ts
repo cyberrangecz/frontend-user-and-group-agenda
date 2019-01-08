e52d56798b986bc387c0e09af8a357e751890455
hamernik <445720@mail.muni.cz>
2019-01-08 13:42:59 +0100

    Add control components and management services

---
+++ src/app/components/group/group-management/group-controls/group-controls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-controls',
+  templateUrl: './group-controls.component.html',
+  styleUrls: ['./group-controls.component.css']
+})
+export class GroupControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

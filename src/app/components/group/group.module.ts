ba0748a02d6004efe34de3766a00f9c466931708
hamernik <445720@mail.muni.cz>
2019-01-08 11:13:00 +0100

    Add base user and group components

--- src/app/components/group/group.module.ts
+++ src/app/components/group/group.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GroupMaterialModule} from './group-material.module';
+import { GroupManagementComponent } from './group-management/group-management.component';
+import { GroupTableComponent } from './group/management/group-table/group-table.component';
 
 @NgModule({
   declarations: [
-  ],
+  GroupManagementComponent,
+  GroupTableComponent],
   imports: [
     CommonModule,
     GroupMaterialModule
--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UserMaterialModule} from './user-material.module';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { UserTableComponent } from './user-management/user-table/user-table.component';
 
 @NgModule({
   declarations: [
 
-  ],
+  UserManagementComponent,
+
+  UserTableComponent],
   imports: [
     CommonModule,
     UserMaterialModule
   ],
   providers: [
-
   ]
 })
 export class UserModule {

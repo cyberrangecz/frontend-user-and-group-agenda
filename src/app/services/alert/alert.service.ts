7a2fd25d08c562b68c132e4b423a9d700a963083
hamernik <445720@mail.muni.cz>
2019-01-21 14:39:29 +0100

    Fix alert service

--- src/app/services/alert/alert.service.ts
+++ src/app/services/alert/alert.service.ts
@@ -2,44 +2,36 @@ import {Injectable} from '@angular/core';
 import {Alert} from '../../model/alert.model';
 import {MatSnackBar} from '@angular/material';
 import {AlertComponent} from '../../components/alert/alert.component';
+import {Queue} from 'typescript-collections';
 
 @Injectable()
 export class AlertService {
 
-  private alertQueue: Alert[] = [];
+  private alertQueue: Queue<Alert> = new Queue<Alert>();
 
   constructor(public snackBar: MatSnackBar) {
 
   }
 
   addAlert(alert: Alert) {
-    this.enqueue(alert);
-    if (this.alertQueue.length <= 1) {
+    this.alertQueue.enqueue(alert);
+    if (this.alertQueue.size() === 1) {
       this.displayAlert();
     }
   }
 
   private displayAlert() {
-    const alert = this.dequeue();
+    const alert = this.alertQueue.peek();
     const snackBarRef = this.snackBar.openFromComponent(AlertComponent, {
       data: alert
     });
 
     snackBarRef.afterDismissed().subscribe(() => {
-      if (this.alertQueue.length > 0) {
+      this.alertQueue.dequeue();
+      if (!this.alertQueue.isEmpty()) {
         this.displayAlert();
       }
     });
-  }
-
-  private dequeue(): Alert {
-    const result = this.alertQueue[0];
-    this.alertQueue = this.alertQueue.splice(0, 1);
-    return result;
-  }
-
-  private enqueue(alert: Alert) {
-    this.alertQueue.push(alert);
   }
 }
 

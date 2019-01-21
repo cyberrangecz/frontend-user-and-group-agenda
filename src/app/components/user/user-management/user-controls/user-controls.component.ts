d8504b76b32956dbef6a66920985f32abd7228f8
hamernik <445720@mail.muni.cz>
2019-01-21 14:49:38 +0100

    Implement user controls component

--- src/app/components/user/user-management/user-controls/user-controls.component.ts
+++ src/app/components/user/user-management/user-controls/user-controls.component.ts
@@ -1,29 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserManagementService} from '../../../../services/user/user-management.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user-controls',
   templateUrl: './user-controls.component.html',
   styleUrls: ['./user-controls.component.css']
 })
-export class UserControlsComponent implements OnInit {
-  selectedUsersCount: number;
+export class UserControlsComponent implements OnInit, OnDestroy {
+
+  private _selectedUsersSubscription: Subscription;
+  selectedUsersCount = 0;
 
   constructor(private userManagementService: UserManagementService) {
   }
 
   ngOnInit() {
+    this.subscribeSelectedUsers();
   }
 
-  addAccount() {
+  ngOnDestroy() {
+    if (this._selectedUsersSubscription) {
+      this._selectedUsersSubscription.unsubscribe();
+    }
+  }
 
+  createAccount() {
+    // TODO popup
+    this.userManagementService.createUser(null);
   }
 
   deleteAccounts() {
-
+    this.userManagementService.deleteSelectedUsers();
   }
 
   synchronizeAccounts() {
+    this.userManagementService.synchronizeSelectedUsers();
+  }
 
+  private subscribeSelectedUsers() {
+    this._selectedUsersSubscription = this.userManagementService.selectionChange$
+      .subscribe(users =>
+        this.selectedUsersCount = users.length);
   }
 }

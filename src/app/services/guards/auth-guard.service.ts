36f4fbe8c287faeecfc1288683c91b6ccb91488e
hamernik <445720@mail.muni.cz>
2019-02-20 12:21:17 +0100

    Fix login issue by removing cyclical redirect in auth guard

--- src/app/services/guards/auth-guard.service.ts
+++ src/app/services/guards/auth-guard.service.ts
@@ -2,20 +2,27 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthService} from '../auth/auth.service';
+import {OAuthService} from 'angular-oauth2-oidc';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService,
+  constructor(private oAuthService: OAuthService,
               private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.isAuthenticated()) {
-      return true;
+    if (this.oAuthService.hasValidIdToken() && this.oAuthService.hasValidAccessToken()) {
+      return Promise.resolve(true);
     }
-    this.router.navigate(['login']);
+    return this.oAuthService.loadDiscoveryDocumentAndLogin()
+      .then(() => {
+        return this.oAuthService.hasValidIdToken() && this.oAuthService.hasValidAccessToken();
+      })
+      .then(valid => {
+        return valid;
+      });
   }
 
 }

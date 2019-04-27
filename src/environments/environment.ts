e4829141be67e8a501f1eae214719d6c94c74f56
Martin Hamerník <445720@mail.muni.cz>
2019-04-27 11:31:29 +0200

    Update OIDC configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin,
   // The SPA's id. The SPA is registered with this id at the auth-server
-  clientId: '3693320b-6acb-442c-be51-86e18f574f9d',
+  clientId: 'b53f2660-8fa0-4d32-94e4-23a59d7e7077',
   // set the scope for the permissions the client should request
   scope: 'openid profile email',
   sessionChecksEnabled: false,

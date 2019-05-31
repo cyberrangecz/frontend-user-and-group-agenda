2b0f5b8bf7356d4d01a17d6592abfdba597a7d82
hamernik <445720@mail.muni.cz>
2019-05-31 09:41:25 +0200

    Use users' names without academic titles

--- projects/user-and-group-management/src/lib/model/DTO/user/user-dto.model.ts
+++ projects/user-and-group-management/src/lib/model/DTO/user/user-dto.model.ts
@@ -2,6 +2,8 @@ import {RoleDTO} from '../role-dto.model';
 
 export class UserDTO {
   full_name: string;
+  given_name: string;
+  family_name: string;
   id: number;
   login: string;
   mail: string;
--- projects/user-and-group-management/src/lib/model/user/user.model.ts
+++ projects/user-and-group-management/src/lib/model/user/user.model.ts
@@ -3,6 +3,7 @@ import {Role} from '../role/role.model';
 export class User {
   id: number;
   name: string;
+  nameWithAcademicTitles: string;
   login: string;
   mail: string;
   roles: Role[];

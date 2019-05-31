f5c630b7548eda6e1a1659bc7802e62adf20328c
hamernik <445720@mail.muni.cz>
2019-05-31 11:41:27 +0200

    Add roles description expandable row to role tables

--- projects/user-and-group-management/src/lib/components/user/user-management/user-table/user-roles-dialog/user-roles-dialog.component.ts
+++ projects/user-and-group-management/src/lib/components/user/user-management/user-table/user-roles-dialog/user-roles-dialog.component.ts
@@ -1,15 +1,25 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef, MatTableDataSource} from '@angular/material';
 import {Role} from '../../../../../model/role/role.model';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {GroupTableDataModel} from '../../../../../model/table-data/group-table-data.model';
 
 @Component({
   selector: 'kypo2-user-roles-dialog',
   templateUrl: './user-roles-dialog.component.html',
-  styleUrls: ['./user-roles-dialog.component.css']
+  styleUrls: ['./user-roles-dialog.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class UserRolesDialogComponent implements OnInit {
 
   displayedColumns = ['name', 'microservice'];
+  expandedRow: Role;
   dataSource: MatTableDataSource<Role>;
 
   constructor(public dialogRef: MatDialogRef<UserRolesDialogComponent>,
--- projects/user-and-group-management/src/lib/model/DTO/role-dto.model.ts
+++ projects/user-and-group-management/src/lib/model/DTO/role-dto.model.ts
@@ -3,4 +3,5 @@ export class RoleDTO {
   name_of_microservice: string;
   id_of_microservice: number;
   role_type: string;
+  description: string;
 }

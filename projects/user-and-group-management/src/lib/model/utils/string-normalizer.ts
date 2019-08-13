ad4e8f9d58b1dd8a0e0213d9673e1a6f48ffd495
hamernik <445720@mail.muni.cz>
2019-08-13 10:49:20 +0200

    Fix packagr build issue

--- projects/user-and-group-management/src/lib/model/utils/string-normalizer.ts
+++ projects/user-and-group-management/src/lib/model/utils/string-normalizer.ts
@@ -1,6 +1,6 @@
 export class StringNormalizer {
-
   static normalizeDiacritics(str: string): string {
-    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    const result = str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // split to declaration and return because of packagr issue
+    return result;
   }
 }

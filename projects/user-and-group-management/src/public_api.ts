adfba730ae0a01078e62db7961f3ae319ed9b3f4
hamernik <445720@mail.muni.cz>
2019-08-13 12:08:48 +0200

    Update public api

--- projects/user-and-group-management/src/public_api.ts
+++ projects/user-and-group-management/src/public_api.ts
@@ -7,4 +7,7 @@ export {UserAndGroupManagementModule} from './lib/components/user-and-group-mana
 export {UserModule} from './lib/components/user/user.module';
 export {GroupModule} from './lib/components/group/group.module';
 export {Kypo2UserAndGroupNotificationService} from './lib/services/alert/kypo2-user-and-group-notification.service';
+export {GroupManagementComponent} from './lib/components/group/group-management/group-management.component';
+export {UserManagementComponent} from './lib/components/user/user-management/user-management.component';
+
 

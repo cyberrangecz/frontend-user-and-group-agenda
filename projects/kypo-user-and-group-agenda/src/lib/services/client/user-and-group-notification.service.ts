3bd274a0feb02671256d72472cf01fc93315a86f
Martin Hamerník <445720@mail.muni.cz>
2020-04-23 13:31:25 +0200

    Add support for follow up action on error or notification

--- projects/kypo-user-and-group-agenda/src/lib/services/client/user-and-group-notification.service.ts
+++ projects/kypo-user-and-group-agenda/src/lib/services/client/user-and-group-notification.service.ts
@@ -1,12 +1,16 @@
 /**
  * Service emitting notification from user and group library
  */
+import { Observable } from 'rxjs';
+
 export abstract class UserAndGroupNotificationService {
   /**
    * Emits notification
    * @param type type of notification
    * @param message message of notification
-   * @param duration optional duration of the notification
+   * @param action name of the action button displayed in the notification
+   *  Returns observable.
+   *  Value of the observable is true if the provided action was selected, false otherwise (no reaction or dismissed)
    */
-  abstract emit(type: 'error' | 'warning' | 'info' | 'success', message: string, duration?: number): void;
+  abstract emit(type: 'error' | 'warning' | 'info' | 'success', message: string, action?: string): Observable<boolean>;
 }
--- src/app/services/client-error-handler.service.ts
+++ src/app/services/client-error-handler.service.ts
@@ -1,9 +1,31 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  CsirtMuNotification,
+  CsirtMuNotificationResult,
+  CsirtMuNotificationService,
+  CsirtMuNotificationTypeEnum,
+} from 'csirt-mu-layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClientErrorHandlerService {
-  emit(err: HttpErrorResponse, operation: string): void {
-    console.log(`${err.message} ${operation}`);
+  constructor(private notificationService: CsirtMuNotificationService) {}
+
+  emit(err: HttpErrorResponse, operation: string, action?: string): Observable<boolean> {
+    const notification: CsirtMuNotification = {
+      type: CsirtMuNotificationTypeEnum.Error,
+      title: operation,
+      source: 'User And Group Agenda',
+    };
+    if (action !== undefined) {
+      notification.action = action;
+    }
+    notification.additionalInfo = [err.error.message];
+
+    return this.notificationService
+      .emit(notification)
+      .pipe(map((result) => result === CsirtMuNotificationResult.CONFIRMED));
   }
 }

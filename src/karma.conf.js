cd1605f0a0373434bc02f51d69914a9a2e90f24c
Karolína Dočkalová Burská <396296@mail.muni.cz>
2023-11-06 15:30:10 +0100

    Adjust code coverage reporter

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
         ]
       }
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
